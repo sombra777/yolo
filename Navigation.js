0939b8bf00a07653c9dd2b76d3a44d2b18f26aab
Gustavo Alvarez <alvarezgustavon7@gmail.com>
2022-04-13 15:46:09 -0500

    first commit

---
+++ Navigation.js
@@ -0,0 +1,66 @@
+import react from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+
+//screens
+import HomeScreen from "./screens/HomeScreen";
+import SettingsScreen from "./screens/SettingdScreen";
+import StackScreen from "./StackScreen";
+
+const Tab = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+
+function MyStack() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: "green",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="home-variant"
+              size={24}
+              color={color}
+            />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="md-settings-sharp" size={24} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
